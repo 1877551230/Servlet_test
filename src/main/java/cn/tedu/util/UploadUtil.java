f73a337b047af6a14c9e9f7ed19f1e36a3852561
miracle <mj0312@vip.qq.com>
2019-08-21 16:56:52 +0800

    自动修改上传的头像为username_headimage.xxx

--- src/main/java/cn/tedu/util/UploadUtil.java
+++ src/main/java/cn/tedu/util/UploadUtil.java
@@ -77,9 +77,12 @@ public class UploadUtil {
 				    	long sizeInBytes=item.getSize();//获取文件的长度
 				    	System.out.println(fieldName+" "+fileName+" "+contentType+" "+isInMemory+" "+sizeInBytes);
 				    	//处理获取真实的文件名,因为浏览器的差异,导致文件名前带有路径,用此功能去掉路径
-				    	if(fileName!=null){
+				    	if(fileName!=null&&fileName!=""){
 				    		fileName=FilenameUtils.getName(fileName);
+				    		fileName=user.getName()+"_headimage."+fileName.split("\\.")[1];
 				    		user.setHeadimage(fileName);
+				    	}else{
+				    		fileName="default.png";
 				    	}
 				    	if(fileTypes.contains(contentType)){
 				    		if(sizeInBytes<4194304){
@@ -115,95 +118,4 @@ public class UploadUtil {
 		}
 		return user;
 	}
-	
-	
-	/*public  void uploadUserHead(HttpServletRequest request, HttpServletResponse response){
-		try {
-			fileTypes.add("image/png");
-			fileTypes.add("image/jpg");
-			fileTypes.add("image/jpeg");
-			fileTypes.add("image/bmp");
-			response.setContentType("text/html;charset=utf-8");
-			PrintWriter out=response.getWriter();
-			// Check that we have a file upload request
-			// 检查我们有一个文件上传请求
-			boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-			System.out.println(isMultipart);
-			if(isMultipart){
-				//说明request中有一个文件
-				// Create a factory for disk-based file items
-				//创建一个磁盘文件条目工厂,工厂生产对象
-				DiskFileItemFactory factory = new DiskFileItemFactory();
-				// Create a new file upload handler
-				ServletFileUpload upload = new ServletFileUpload(factory);
-				// Parse the request
-				//把request中的数据,转换成list集合,元素类型为fileItem
-				List<FileItem> items = upload.parseRequest(request);
-				// Process the uploaded items
-				//处理上传的条目
-				Iterator<FileItem> iter = items.iterator();
-				while (iter.hasNext()) {
-				    FileItem item = iter.next();
-
-				    if (item.isFormField()) {
-				    	//是非文件域,除了文件之外的input
-				        String name=item.getFieldName();//<input name>
-				        String value=item.getString();//<input value>
-				    } else {
-				    	//是文件域<input type="file"processUploadFile(item)
-				    	String fieldName=item.getFieldName();
-				    	//String fileName=item.getName();//获取真实文件名
-				    	String contentType=item.getContentType();//获取文件内容类型
-				    	boolean isInMemory=item.isInMemory();//是否在内存中
-				    	long sizeInBytes=item.getSize();//获取文件的长度
-				    	//处理获取真实的文件名,因为浏览器的差异,导致文件名前带有路径,用此功能去掉路径
-				    	String fileRealName=item.getName();
-				    	System.out.println(fieldName+" "+fileRealName+" "+contentType+" "+isInMemory+" "+sizeInBytes);
-				    	System.out.println(fileRealName);
-				    	if(sizeInBytes<4194304){
-				    		if(sizeInBytes==0){
-				    			fileName="default.png";
-				    			break;
-				    		}
-				    		if(fileTypes.contains(contentType)){
-				    		//fileName=request.getParameter("userName")+"head"+fileRealName.split(".")[1];
-				    		//准备上传文件在服务器的存储路径
-					    	String realPath=request.getServletContext().getRealPath("/upload");
-					    	System.out.println(realPath);
-					    	File file=new File(realPath);
-					    	if(!file.exists()){
-					    		file.mkdir();
-					    	}
-					    	//把文件上传到upload文件夹
-					    	File uploadFile=new File(file,fileName);
-					    	item.write(uploadFile);//把文件写到具体路径
-					    	out.println("<div style='color:red;'>上传文件成功</div>");
-				    		}else{
-				    			out.println("<div style='color:red;'>文件太大</div>");
-				    		}
-				    	}else{
-				    		out.println("<div style='color:red;'>文件类型不匹配</div>");
-				    	}
-				    	
-				    }
-				    
-				}
-			}else{
-				fileName="default.png";
-			}
-			out.flush();
-			out.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public String getFileName() {
-		return fileName;
-	}
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}*/
-	
-
 }
